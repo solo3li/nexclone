a265e2542a88fcd09ffede5ebd7d573fcb5892f1
solo3li <hamed3alii.3@gmail.com>
2026-06-19 00:04:45 +0000

    الفرونت شغال فل الفل

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = { allowedDevOrigins: ['178.62.192.74', 'localhost'] };
 
 export default withNextIntl(nextConfig);
