e0da6d3df74fe156662ae9a2cb2227408f459303
solo3li <hamed3alii.3@gmail.com>
2026-06-21 00:11:14 +0000

    chore: remove PricingSection component from homepage

--- frontend/app/[locale]/page.tsx
+++ frontend/app/[locale]/page.tsx
@@ -37,7 +37,6 @@ export default function Page() {
           <ToolsSection />
           <HowItWorks />
           <FeaturesSection />
-          <PricingSection />
           <CTASection />
         </main>
         <Footer />
