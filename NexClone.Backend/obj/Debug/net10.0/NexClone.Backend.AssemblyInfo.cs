5f070d8958d6debf4c4fee8f5169a10e7ac94e5a
solo3li <hamed3alii.3@gmail.com>
2026-08-14 23:49:33 +0300

    build: generate bin and obj artifacts for NexClone.Backend

--- NexClone.Backend/obj/Debug/net10.0/NexClone.Backend.AssemblyInfo.cs
+++ NexClone.Backend/obj/Debug/net10.0/NexClone.Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NexClone.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33610dcd8624d2b225c9a1474e2ba5ef1595ad63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c773afbcd13b282de05faa6d8e6f0f1dfb09e248")]
 [assembly: System.Reflection.AssemblyProductAttribute("NexClone.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NexClone.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
