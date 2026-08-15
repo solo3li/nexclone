4f42e76a2579a1a95f8fec79f5d929239c3c61a9
solo3li <hamed3alii.3@gmail.com>
2026-08-15 22:15:43 +0300

    feat: configure next-intl, CORS policies, service worker headers, and API rewrites in next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,7 +4,7 @@ const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ['178.62.192.74', 'localhost', 'uri-kilometers-believed-palmer.trycloudflare.com'],
+  allowedDevOrigins: ['178.62.192.74', 'localhost', 'filters-leisure-marion-healthcare.trycloudflare.com'],
   typescript: {
     ignoreBuildErrors: true,
   },
