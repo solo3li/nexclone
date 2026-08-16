e3577ec0b2c198cdcbea71554ef398ac1cadf2a7
solo3li <hamed3alii.3@gmail.com>
2026-08-16 03:04:19 +0300

    feat: configure next-intl, custom headers, and API rewrites in next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,7 +4,12 @@ const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ['178.62.192.74', 'localhost', 'filters-leisure-marion-healthcare.trycloudflare.com'],
+  allowedDevOrigins: [
+    '178.62.192.74', 
+    'localhost', 
+    'filters-leisure-marion-healthcare.trycloudflare.com',
+    'brass-chevy-remarkable-amazing.trycloudflare.com'
+  ],
   typescript: {
     ignoreBuildErrors: true,
   },
