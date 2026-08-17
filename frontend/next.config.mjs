7721adc4d0ee5fad02c3f7eb7c7ca94436b9c599
solo3li <hamed3alii.3@gmail.com>
2026-08-17 23:57:26 +0300

    chore: add system log file and Next.js configuration file

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -6,10 +6,10 @@ const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 const nextConfig = {
   allowedDevOrigins: [
     '178.62.192.74', 
-    'localhost', 
-    'filters-leisure-marion-healthcare.trycloudflare.com',
-    'lab-contain-meal-density.trycloudflare.com',
-    'guys-industry-exercise-suffering.trycloudflare.com',
+    'localhost',
+    '127.0.0.1',
+    '*.trycloudflare.com',
+    'redhead-emerald-boys-started.trycloudflare.com',
   ],
   typescript: {
     ignoreBuildErrors: true,
