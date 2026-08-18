a67777901976ba1797693add37dac674b0900644
solo3li <hamed3alii.3@gmail.com>
2026-08-18 23:32:50 +0300

    fix: clear stale cloudflare tunnel logs and add build artifacts for backend project

--- NexClone.Backend/obj/Debug/net10.0/NexClone.Backend.AssemblyInfo.cs
+++ NexClone.Backend/obj/Debug/net10.0/NexClone.Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NexClone.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c5e84340cb0b41c92d82ef4bf9f7bf54e95f407")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+168059b8e98e7dfc50d43624e570ca956cbd72de")]
 [assembly: System.Reflection.AssemblyProductAttribute("NexClone.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NexClone.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
