c41dbcfb9a0aafee5845fff1157d9c4a07bb3135
solo3li <hamed3aliii.3@gmail.com>
2026-08-20 14:18:34 +0200

    chore: update build artifacts and logging configuration

--- NexClone.Backend/obj/Debug/net10.0/NexClone.Backend.AssemblyInfo.cs
+++ NexClone.Backend/obj/Debug/net10.0/NexClone.Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NexClone.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa5b5689cefbb43c58174a694536e0f1bc5e178c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+add027dca9c55a2924bb5b3d8d5dff1f0bd8c505")]
 [assembly: System.Reflection.AssemblyProductAttribute("NexClone.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NexClone.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
