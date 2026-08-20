aa7b17b9368e07a912ffdaed97cbea6efcb669ce
solo3li <hamed3aliii.3@gmail.com>
2026-08-20 07:17:13 +0200

    chore: add web entrypoint to traefik, update backend host binding to 0.0.0.0, and configure local kubernetes routing.

--- NexClone.Backend/obj/Debug/net10.0/NexClone.Backend.AssemblyInfo.cs
+++ NexClone.Backend/obj/Debug/net10.0/NexClone.Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NexClone.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+120effe2ccd2d1a20eb3b1d6875a52096bbb95d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fec4b4465d16bd963cfc0038c73d96a6beb2a150")]
 [assembly: System.Reflection.AssemblyProductAttribute("NexClone.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NexClone.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
