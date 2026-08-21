b860b7e91a54fcf9758706b000758a00cea7cc59
solo3li <hamed3aliii.3@gmail.com>
2026-08-21 23:56:32 +0200

    feat: update system log with server removal event and refresh source link reference

--- NexClone.Backend/obj/Debug/net10.0/NexClone.Backend.AssemblyInfo.cs
+++ NexClone.Backend/obj/Debug/net10.0/NexClone.Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NexClone.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8b132981ad5d0fd0bc84bcfc3a958ed195aa5e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f15cfbd1bbbb7d4a2165be3c44c6d8c70b62bb90")]
 [assembly: System.Reflection.AssemblyProductAttribute("NexClone.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NexClone.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
