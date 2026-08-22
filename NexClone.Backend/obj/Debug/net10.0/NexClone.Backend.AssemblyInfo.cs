2f4dca2806eb514493ebfa6255a4e91ddb6df5ef
solo3li <hamed3aliii.3@gmail.com>
2026-08-23 01:04:10 +0200

    build: update assembly metadata and regenerate project cache files

--- NexClone.Backend/obj/Debug/net10.0/NexClone.Backend.AssemblyInfo.cs
+++ NexClone.Backend/obj/Debug/net10.0/NexClone.Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NexClone.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ace90c90e61698d223f0704cd280bdb2e6bdd2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc9d02c7928884d52005e05be594a0f8ca8dcfee")]
 [assembly: System.Reflection.AssemblyProductAttribute("NexClone.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NexClone.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
